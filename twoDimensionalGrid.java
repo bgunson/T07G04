d4e0089095cf1b98ff788e25307fc03ec3d71a10
Jan-Jelinek <47360894+Jan-Jelinek@users.noreply.github.com>
2019-02-19 16:02:30 -0700

    Update twoDimensionalGrid.java

--- twoDimensionalGrid.java
+++ twoDimensionalGrid.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class twoDimensionalGrid 
 {
-	int rows = 10;
-	int columns = 10;
-	String[][] grid = new String[columns][rows];
+	private int rows = 10;
+	private int columns = 10;
+	public String[][] grid = new String[columns][rows];
 
 	/*
 	gridBuilder sets up the initial grid for use later on
@@ -21,14 +21,49 @@ public class twoDimensionalGrid
 	public void gridDisplay()
 	{
 		
-		for(int i = 0; i<rows; i++)
+		for(int i = 0; i<columns; i++)
 		{
-			for(int j = 0; j<columns; j++)
+			for(int j = 0; j<rows; j++)
 			{
-				System.out.print(grid[i][j]);
+				System.out.print(grid[j][i]);
 				System.out.print(" ");
 			}
 			System.out.println();
+		}
+	}
+
+	/*
+	placeShip plcaes a ship of given width and height at given point
+	params x and y are the top left x and y position of ship
+	paras width, height are width and height of ship
+	*/
+	public void placeShip(int x, int y, int width, int height){
+		
+		// if placing out of grid
+		if (( x + width > columns) || (y + height > rows)){ 
+			System.out.println("invalid placement");
+		}
+		else{
+			// check if ship already exists in spot
+			boolean spaceAvailable = true;
+			for(int i = x; i < (x+width); i++){
+				for(int j = y; j < (y+height); j++){
+					if (grid[i][j] != "O"){
+						System.out.println("invalid placement");
+						spaceAvailable = false;
+					}
+				}
+			}
+			
+			// places the ship in spot
+			if (spaceAvailable){
+				for(int i = x; i < (x+width); i++){
+					for(int j = y; j < (y+height); j++){
+						grid[i][j] = "X";
+					}
+				}
 			}
 		}
+	}
+	
 }
