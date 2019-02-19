bd524777ff63a68227056d352d5ef7bd999641af
ArcaneShift <47360728+ArcaneShift@users.noreply.github.com>
2019-02-18 19:27:51 -0700

    Add files via upload
    Added comments and split up making and displaying the 2d array

--- startProgram.java
+++ startProgram.java
@@ -2,17 +2,10 @@
 public class startProgram {
 	public static void main(String[] args) {
 		
-		
+		//Runs the other class to build and display the 2d array
 		twoDimensionalGrid twoDimensionalGrid = new twoDimensionalGrid();
 		twoDimensionalGrid.gridBuilder();
-		
-		
-		
-		
-		
-		
-		
-		
+		twoDimensionalGrid.gridDisplay();
 		
 	}
 }
--- twoDimensionalGrid.java
+++ twoDimensionalGrid.java
@@ -3,26 +3,32 @@ public class twoDimensionalGrid
 {
 	int rows = 10;
 	int columns = 10;
-	
 	String[][] grid = new String[columns][rows];
 
-	public void gridBuilder() {
-		
-		
+	/*
+	gridBuilder sets up the initial grid for use later on
+	 */
+	public void gridBuilder() 
+	{
 		for(int i = 0; i<rows; i++)
 		    for(int j = 0; j<columns; j++)
 		        grid[i][j] = "O";
-		
-		
-		
-	for(int i = 0; i<rows; i++)
-	{
-	    for(int j = 0; j<columns; j++)
-	    {
-	        System.out.print(grid[i][j]);
-	        System.out.print(" ");
-	    }
-	    System.out.println();
 	}
-}
+	
+	/*
+	gridDisplay prints out the 2d array in an easy to see format
+	 */
+	public void gridDisplay()
+	{
+		
+		for(int i = 0; i<rows; i++)
+		{
+			for(int j = 0; j<columns; j++)
+			{
+				System.out.print(grid[i][j]);
+				System.out.print(" ");
+			}
+			System.out.println();
+			}
+		}
 }
