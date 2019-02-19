f416f355a52a9c12a363cee43861041ab8620c59
Jan-Jelinek <47360894+Jan-Jelinek@users.noreply.github.com>
2019-02-19 16:17:17 -0700

    Update startProgram.java

--- startProgram.java
+++ startProgram.java
@@ -7,5 +7,20 @@ public class startProgram {
 		twoDimensionalGrid.gridBuilder();
 		twoDimensionalGrid.gridDisplay();
 		
+		/* if you wanna test the method for yourself
+		Scanner reader = new Scanner(System.in);
+		for(int q = 2; q>0; q--){
+			System.out.println("\n");
+			
+			System.out.println("input back to back in this order: x, y, width, height");
+			int x = reader.nextInt();
+			int y = reader.nextInt();
+			int w = reader.nextInt();
+			int h = reader.nextInt();
+			
+			twoDimensionalGrid.placeShip(x, y, w, h); 
+			twoDimensionalGrid.gridDisplay();
+		}
+		*/
 	}
 }
