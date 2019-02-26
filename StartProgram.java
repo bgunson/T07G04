5e270d36f10ff171ddcc140b202e60eee6536526
Jan-Jelinek <47360894+Jan-Jelinek@users.noreply.github.com>
2019-02-26 10:27:19 -0700

    Update StartProgram.java

--- StartProgram.java
+++ StartProgram.java
@@ -8,40 +8,66 @@ public class StartProgram {
 		System.out.println("Welcome to BattleShip Galactica!");
 
 		// Create game board for player 1.
-		TwoDimensionalGrid playerOneGrid = new TwoDimensionalGrid();
-		playerOneGrid.gridBuilder();
-		// Create ship fleet for player 1.
-		ShipTeam playerOneShips = new ShipTeam("Player One Fleet");
-		// Prompt player one to select locations for all their ships
-		playerOneShips.placeAllShips(playerOneGrid);
+		twoDimensionalGrid playerOneShips = new twoDimensionalGrid();
+		playerOneShips.gridBuilder();
+		twoDimensionalGrid playerOneHits = new twoDimensionalGrid();
+		playerOneHits.gridBuilder();
+		
+		twoDimensionalGrid playerTwoShips = new twoDimensionalGrid();
+		playerTwoShips.gridBuilder();
+		twoDimensionalGrid playerTwoHits = new twoDimensionalGrid();
+		playerTwoHits.gridBuilder();		
 
-		// Main game loop, one player game shooting at your own board.
-		while (playerOneShips.isEveryShipDestroyed() == false ){
-			System.out.println("");
-			playerOneGrid.gridDisplay();
-			System.out.println("Choose an (x,y) for your next shot: ");
-			// Needs to check if input is an actual coord on the grid.
-			Scanner selection = new Scanner(System.in);
-			char xChar = selection.next().charAt(0);
-			xChar -= 97;
-			int x = (int)(xChar);
-			int y = selection.nextInt();
+		System.out.println("Player 1 place ships");
+		ShipTeam playerOneFleet = new ShipTeam("Player One Fleet");
+		playerOneFleet.placeAllShips(playerOneShips);
 
-			boolean shot = playerOneGrid.shotFired(x, y);
-			if (shot == true){
-				System.out.println("Good Hit!");
-				playerOneShips.goodHit();
+		System.out.println("Player 2 place ships");
+		ShipTeam playerTwoFleet = new ShipTeam("Player Two Fleet");
+		playerOneFleet.placeAllShips(playerTwoShips);
+
+
+		
+		int i = 1;
+		while((playerOneFleet.isEveryShipDestroyed() == false)||(playerTwoFleet.isEveryShipDestroyed() == false)){
+
+			i++;
+			if ( i%2 == 0 ){
+				System.out.println("PLAYER 1 TURN");
+				playTurn(playerOneShips, playerOneHits, playerTwoShips);
 			}
 			else{
-				System.out.println("Miss!");
+				System.out.println("PLAYER 2 TURN");
+				playTurn(playerTwoShips, playerTwoHits, playerOneShips);
 			}
-
-
 		}
-		System.out.println("GAME OVER!");
-
-
-
 	}
 
+	public static void playTurn(twoDimensionalGrid playerShips, twoDimensionalGrid playerHits, twoDimensionalGrid enemyShips){
+			
+		System.out.println("Your Ships");
+		playerShips.gridDisplay();
+		System.out.println("Hit/Misses");
+		playerHits.gridDisplay();
+	
+		System.out.println("Choose an (x,y) for your next shot: ");
+		// Needs to check if input is an actual coord on the grid.
+		Scanner selection = new Scanner(System.in);
+
+		char xChar = selection.next().charAt(0);
+		xChar -= 97;
+		int x = (int)(xChar);
+		int y = selection.nextInt();
+
+		boolean shot = enemyShips.shotFired(x, y);
+		playerHits.placeShot(shot, x, y);
+		
+		if (shot == true){
+			enemyShips.placeShot(shot, x, y);
+			System.out.println("Hit!");
+		}
+		else
+			System.out.println("Miss!");
+		
+	}
 }
