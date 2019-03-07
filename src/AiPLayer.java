1291cf195b6aabd7949be799215e050dda8b7ae1
bgunson <bennett.gunson@ucalgary.ca>
2019-03-07 13:24:44 -0700

    Create AiPLayer.java

---
+++ src/AiPLayer.java
@@ -0,0 +1,18 @@
+public class AiPlayer{
+
+    /**
+     * Places starting ships.
+     */
+    public void shipPlacer(Board board){
+
+    }
+
+    /**
+     * When called, computer player will shoot on the board (randomly?)...
+     */
+    public void turn(Board board){
+
+    }
+
+
+}
