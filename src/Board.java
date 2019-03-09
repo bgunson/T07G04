c12f1129a59c7a7a7794beaa9ba58d38bb8920ec
Jan-Jelinek <47360894+Jan-Jelinek@users.noreply.github.com>
2019-03-08 17:25:04 -0700

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -4,23 +4,26 @@ public class Board
 {
 	private int rows = 10;
 	private int columns = 10;
+	
 	public String[][] grid = new String[columns][rows];
-
-	/*
-	gridBuilder sets up the initial grid for use later on
+	/* Maybe something like this?
+	 * " "  = empty
+	 * "G" = ship part in good condition
+	 * "D" = ship part is damaged
+	 * "F" = Ship destroyed
 	 */
-	public void gridBuilder() 
-	{
-		for(int i = 0; i<rows; i++)
+	
+	// Creates and fills out the empty board
+	public Board(){ // just changed gridBuilder into this
+		for(int i = 0; i<rows; i++){
 		    for(int j = 0; j<columns; j++)
-		        grid[i][j] = "O";
+		        grid[i][j] = " ";
+		}
 	}
 	
-	/*
-	gridDisplay prints out the 2d array in an easy to see format
-	 */
-	public void gridDisplay()
-	{
+	// temp will replace with GUI
+	public void boardDisplay(){
+		
 		System.out.println("  A B C D E F G H I J");
 		for(int i = 0; i<columns; i++)
 		{
@@ -36,14 +39,46 @@ public class Board
 	
 	
 	/*
-	placeShip plcaes a ship of given width and height at given point
+	On the map, ships that sink will be a different color so this changes
+	it from being a bunch of destroyed parts to sunken parts
+	["D","D","G"] --> gets hit --> ["D","D","D"] --> ["F","F","F"]	
+	*/
+	public void updateDestroyedShips( ShipTeam shipTeam ){
+		
+		ArrayList<Ship> fleet = shipTeam.getShips();
+		
+		for ( Ship s : fleet ){
+			if (s.sunk == true){
+				for ( String a : s.getCoordinates() ){
+					grid[(int)a.charAt(0) - 97][(int)a.charAt(1) - 97] = "F"; // haven't tested if works
+				}
+			}
+		}
+	}
+		
+	/*
+	placeShip places a ship of given width and height at given point
 	params x and y are the top left x and y position of ship
 	paras width, height are width and height of ship
 	*/
-	public boolean placeShip(int x, int y, int width, int height){
+	public boolean placeShip(int x, int y, int length, int orientation){
+		/*
+			Also once placed: ship.placedShip( String[] wherePlaced )
+		*/
+		int height, width;
+		// 1 = horizontal, 2 = vertical
+		if (orientation == 1){
+			width = length;
+			height = 1;
+		}
+		else{
+			width = 1;
+			height = length;			
+		}
 		
 		// if placing out of grid
 		if (( x + width > columns) || (y + height > rows)){ 
+			//System.out.println("out of grid");
 			// Bad placement.
 			return false;
 		}
@@ -52,19 +87,17 @@ public class Board
 			boolean spaceAvailable = true;
 			for(int i = x; i < (x+width); i++){
 				for(int j = y; j < (y+height); j++){
-					if (grid[i][j] != "O"){
+					if (grid[i][j] != " "){
 						spaceAvailable = false;
 						return false;
 					}
 				}
-			}
-
-			
-			// places the shpi in spot
+			}			
+			// places the ship in spot
 			if (spaceAvailable){
 				for(int i = x; i < (x+width); i++){
 					for(int j = y; j < (y+height); j++){
-						grid[i][j] = "X";
+						grid[i][j] = "G";
 					}
 				}
 				// Good placemnet.
@@ -73,7 +106,6 @@ public class Board
 		}
 		return false;
 	}
-
 
 	/**
 	 * This method prompts the user to place a ship either vertically or horizontally at a specified keyboard
@@ -85,7 +117,7 @@ public class Board
 
 		System.out.println("Choosing location for the " + shipName + "...");
 		System.out.println(shipName + " specifications: ");
-		System.out.println("LENGTH = " + shipToPlace.getShipLength() + " WIDTH = " + shipToPlace.getShipWidth());
+		System.out.println("LENGTH = " + shipToPlace.shipLength);
 		System.out.println("Horizontal [1] or Vertical [2] ?");
 		Scanner keyboard = new Scanner(System.in);
 		int orientation = keyboard.nextInt();
@@ -104,11 +136,11 @@ public class Board
 
 			// If wanted vertical place ship (x, y, L, W).
 			if (orientation == 1){
-				wasGoodPlace = placeShip(x, y, shipToPlace.getShipLength(), shipToPlace.getShipWidth());
+				wasGoodPlace = placeShip(x, y, shipToPlace.shipLength, 1);
 			}
-			// Else vertical, place  ship (x, y, W, L).
+			// Else vertical, place ship (x, y, W, L).
 			else{
-				wasGoodPlace = placeShip(x, y, shipToPlace.getShipWidth(), shipToPlace.getShipLength());
+				wasGoodPlace = placeShip(x, y, shipToPlace.shipLength, 2);
 			}
 
 			// Check wasGoodPlace condition.
@@ -122,6 +154,7 @@ public class Board
 			}
 		}
 	}
+	
 
 	/**
 	 * shotFired takes an x,y position on the given grid (opponent) and checks if the shot was ahit or miss and
@@ -131,7 +164,7 @@ public class Board
 	 * @return, true if the shot was a hit, false if not.
 	 */
 	public boolean shotFired(int x, int y){
-		if (grid[x][y] == "X"){
+		if (grid[x][y] == "G"){
 			return true;
 		}
 		return false;
@@ -140,8 +173,45 @@ public class Board
 	public void placeShot(boolean hit, int x, int y){
 		if (hit == true)
 			grid[x][y] = "H";
+		// update Ship that got hit
 		else
 			grid[x][y] = "M";
 	}
+	
+	
+	
+	/*
+		Ignore, idk probs won't use
+		
+		
+		
+		Updates the board based on the coordinates and destroyed arrays
+		for each ship in ShipTeam
+	
+	public void updateBoard(ShipTeam shipTeam){
+		
+		ArrayList<Ship> fleet = shipTeam.getShips();
+		
+		for (Ship ship : fleet){
+			String[] coordinates = ship.getCoordinates();
+				
+			// if ship sunk
+			if (ship.sunk == true){
+				for (String coordinate : coordinates)
+						grid[(int)(coordinate.substring(0,1) -= 97)][coordinate.substring(1,2)] = "F";
+			}
+			// if ship still alive	
+			else{
+				for (String coordinate : coordinates)
+						grid[(int)(String.valueOf(coordinate.substring(0,1) -= 97))][String.parseInt(coordinate.substring(1,2))] = "G";
+				String[] destroyed = ship.getDestroyed();			
+				for (String destroyedTile : destroyed)
+					grid[destroyedTile.substring(0,1)) -= 97][Integer.parseInt(destroyedTile.substring(1,2))] = "D";				
+			// char c=s.charAt(0)
+			}
+		}
+			
+	}
+	*/
 
 }
