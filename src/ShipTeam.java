1eff07c4f789860a6144207081a6fe0cf4ab8e62
Jan-Jelinek <47360894+Jan-Jelinek@users.noreply.github.com>
2019-03-08 17:26:26 -0700

    Update ShipTeam.java

--- src/ShipTeam.java
+++ src/ShipTeam.java
@@ -3,50 +3,42 @@ import java.util.ArrayList;
 public class ShipTeam{
 
     private ArrayList<Ship> shipFleet;
-    private String name;
-    int amountHit;
-
+	private String name;
+	
     /**
      * Constructs a ship team with a given name for the fleet. Then adds the 5 default ships with names according
-     * to the player that the fleet belongs to.
-     * @param name, the string tha the fleet is to be named when constructed.
      */
     public ShipTeam(String name){
 
         this.name = name;
-        amountHit = 0;
+        //amountHit = 0;
         shipFleet = new ArrayList<Ship>();
         // Add default ships with name <shipName (player fleet name)>
-        shipFleet.add(new Ship(5, "Spacestation"));
-        shipFleet.add(new Ship(4, "BattleShip"));
+        //shipFleet.add(new Ship(5, "Spacestation"));
+        //shipFleet.add(new Ship(4, "BattleShip"));
         //shipFleet.add(new Ship(3, "Cruiser"));
         shipFleet.add(new Ship(3, "Destroyer"));
         //shipFleet.add(new Ship(2, "Satellite"));
 
+
     }
 
+    public ArrayList<Ship> getShips(){
+        return shipFleet;
+    }
+	
     /**
      * Iterates through each ship in the fleet and promts human player to choose location for the specifeid ship.
      * @param grid, the game board for the players grid.
      */
     public void placeAllShips(Board grid){
+		
         for (Ship someShip : shipFleet){
-            grid.gridDisplay();
-            grid.promptToPlaceShip(someShip, someShip.getShipName());
+            grid.boardDisplay();
+            grid.promptToPlaceShip(someShip, someShip.name);
             System.out.println("");
         }
-    }
-
-    public ArrayList<Ship> getShips() {
-        return shipFleet;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void goodHit(){
-        this.amountHit += 1;
+        
     }
 
     /**
@@ -55,13 +47,6 @@ public class ShipTeam{
      * @return true if all ships in fleet are destoyed, false otherwise.
      */
     public boolean isEveryShipDestroyed(){
-        int totalHitsAllowed = 0;
-        for (Ship s : shipFleet){
-            totalHitsAllowed += s.getShipLength();
-        }
-        if (totalHitsAllowed == amountHit)
-            return true;
-        else
-            return false;
+        return false;
     }
 }
