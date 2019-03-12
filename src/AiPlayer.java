5a5653829146ff520d66bf2a03f5398d48bb80d1
bgunson <47361247+bgunson@users.noreply.github.com>
2019-03-12 09:30:11 -0600

    Update AiPlayer.java

--- src/AiPlayer.java
+++ src/AiPlayer.java
@@ -1,8 +1,29 @@
 public class AiPlayer{
+    
     /**
-     * Places starting ships.
+     * shipPlacer will randomly place the default ships for the computer player n its own board.
+     * @param board the board object representing the AI's ship locations.
+     * @param fleet, the ShipTeam object containing the AI's ship objects.
      */
-    public void shipPlacer(Board board){
+    public void shipPlacer(Board board, ShipTeam fleet){
+
+        ArrayList<Ship> theShips = fleet.getShipFleet();
+
+        int randomX, randomY;
+        for (Ship s : theShips){
+            System.out.println(s.name);
+            boolean goodPlace = false;
+            while (goodPlace == false){
+                Random rand = new Random();
+                randomX = rand.nextInt(10);
+                randomY = rand.nextInt(10);
+                if (randomX % 2 == 0)
+                    goodPlace = board.placeShip(randomX, randomY, s.shipLength, 1);
+                else
+                    goodPlace = board.placeShip(randomX, randomY, s.shipLength, 2);
+            }
+        }
+
 
     }
 
