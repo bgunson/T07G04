78873b36448b8159fc20fd20d5214d42f8bd8410
bgunson <47361247+bgunson@users.noreply.github.com>
2019-03-27 12:43:00 -0600

    Add files via upload

---
+++ gui/SG/src/drivers/BattleshipGalactica.java
@@ -0,0 +1,46 @@
+package drivers;
+
+import gui.FirstScene;
+import gui.ShipPlaceScene;
+import logic.Game;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+/**
+ * TO DO:
+ * - i broke the ai and i don't know how to fix it
+ * - end the game when all ships are sunk
+ * - update visuals
+ * - add labels to explain whats happening
+ */
+
+public class BattleshipGalactica extends Application{
+    Stage stage;
+    Game game = new Game();
+
+    
+    public void setScene(Scene scene){
+        stage.setScene(scene);
+    }
+    
+    public Game getGame(){
+        return game;
+    }
+    
+    public void start(Stage stage){
+        
+        this.stage = stage;
+        stage.setTitle("BATTLESHIP GALACTICA THE IV");
+        stage.show();
+        
+        FirstScene scene = new FirstScene(this);
+        
+        scene.setup();
+    }
+    
+    public static void main(String[] args){
+        launch(args);
+    }
+}
