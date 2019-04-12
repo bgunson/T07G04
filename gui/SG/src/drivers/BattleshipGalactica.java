eab89801669b8f405f73fb3236d0adb6a7192eff
bgunson <bennett.gunson@ucalgary.ca>
2019-04-11 19:56:29 -0600

    JavaDoc for BattleshipGalactica

--- gui/SG/src/drivers/BattleshipGalactica.java
+++ gui/SG/src/drivers/BattleshipGalactica.java
@@ -8,26 +8,44 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/*
+Main driver that acts as a link between our logic and GUI.
+ */
 public class BattleshipGalactica extends Application{
+
+    // Instance of Stage and new Game object
     Stage stage;
     Game game = new Game();
+    // Music
     private AudioController backgroundMusic = new AudioController(BattleshipGalactica.class.getResource("/Resources/Audio/MainMenu.mp3").toExternalForm());
-    
+
+    /**
+     * Set scene with specified scene.
+     */
     public void setScene(Scene scene){
     	
         stage.setScene(scene);
     }
-    
+
+    /**
+     * @return the current game being run.
+     */
     public Game getGame(){
     	backgroundMusic.stop();
         return game;
     }
-    
+
+    /**
+     * Set the current game with a specified one.
+     */
     public void setGame(Game game) {
 
     	this.game = game;
     }
-    
+
+    /**
+     * Start the main driver, excecutes when the program is run.
+     */
     public void start(Stage stage){
 		
 		backgroundMusic.playSong();
@@ -41,7 +59,10 @@ public class BattleshipGalactica extends Application{
         
         scene.setup();
     }
-    
+
+    /**
+     * Main method.
+     */
     public static void main(String[] args){
         launch(args);
     }
