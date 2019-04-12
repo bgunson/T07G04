679ecea36d1c6695e5418d0e9a5171ad42432cc4
bgunson <bennett.gunson@ucalgary.ca>
2019-04-11 20:21:49 -0600

    Javadoc for BtnStartHandler

--- gui/SG/src/handlers/BtnStartHandler.java
+++ gui/SG/src/handlers/BtnStartHandler.java
@@ -9,14 +9,24 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import logic.Game;
 
+/*
+Handles events when the start button is clicked.
+ */
 public class BtnStartHandler implements EventHandler<ActionEvent>{
 
+    // Current session.
     BattleshipGalactica session;
-    
+
+    /**
+     * Constructs a hanlder with the current driver.
+     */
     public BtnStartHandler(BattleshipGalactica session){
         this.session = session;
     }
-    
+
+    /**
+     * When clicked; music is started and application goes to the ship place scene.
+     */
     @Override
     public void handle(ActionEvent event){
     	Game startMusic = new Game();
