b8eab4d2d476052ba57c416614a08653f78b6f1a
bgunson <bennett.gunson@ucalgary.ca>
2019-04-11 20:20:10 -0600

    Javadoc for BtnReplayHandler

--- gui/SG/src/handlers/BtnReplayHandler.java
+++ gui/SG/src/handlers/BtnReplayHandler.java
@@ -6,14 +6,24 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import logic.Game;
 
+/*
+Handles the event when the replay button is quit.
+ */
 public class BtnReplayHandler implements EventHandler<ActionEvent>{
 
+    // Current session.
     private BattleshipGalactica session;
-    
+
+    /**
+     * Construct handler with the current driver.
+     */
     public BtnReplayHandler(BattleshipGalactica session) {
     	this.session = session;
     }
-    
+
+    /**
+     * Via the current session, a new game is initiated and application returns to ship place scene.
+     */
     @Override
     public void handle(ActionEvent event){
     	
