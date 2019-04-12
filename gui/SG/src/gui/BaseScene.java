e73b95bafd8512dfcbae66285f852d1ed5c45d4e
bgunson <bennett.gunson@ucalgary.ca>
2019-04-11 20:05:27 -0600

    Javadoc for BaseScene

--- gui/SG/src/gui/BaseScene.java
+++ gui/SG/src/gui/BaseScene.java
@@ -4,23 +4,41 @@ import Audio.AudioController;
 import drivers.BattleshipGalactica;
 import javafx.scene.Scene;
 
+/*
+Abstract parent class for our scene objects.
+ */
 public abstract class BaseScene {
+
+    // Scene variable and current session instances.
     private Scene scene;
     private BattleshipGalactica battleshipGalatica;
+
+    /**
+     * Constructor that sets the session as the one in progress.
+     */
     public BaseScene(BattleshipGalactica battleshipGalatica){
      this.battleshipGalatica = battleshipGalatica;
     }
-    
+
+    /**
+     * @return the current scene.
+     */
     public Scene getScene(){
     	
         return scene;
     }
-    
+
+    /**
+     * @return the current driver object.
+     */
     public BattleshipGalactica getSession(){
     	
         return battleshipGalatica;
     }
-    
+
+    /**
+     * Set the current scene to a specified Scene.
+     */
     public void setScene(Scene scene){
     	
         this.scene = scene;
@@ -28,9 +46,12 @@ public abstract class BaseScene {
 
     // Sets up the scene
     public abstract void setup();
-    
+
+    /**
+     * Display the scene.
+     */
     protected void display(){
-    	
+
         battleshipGalatica.setScene(this.scene);
     }
 }
