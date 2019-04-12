70b625d7515425096932a1355da80b502fed3d6b
bgunson <bennett.gunson@ucalgary.ca>
2019-04-11 20:17:15 -0600

    Javadoc for BtnShipPlaceHandler

--- gui/SG/src/handlers/BtnPlaceShipHandler.java
+++ gui/SG/src/handlers/BtnPlaceShipHandler.java
@@ -6,18 +6,28 @@ import gui.AttackScene;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
+/*
+Handles the mouse events for placing ships.
+ */
 public class BtnPlaceShipHandler implements EventHandler<ActionEvent>{
 
+    // Current session, and click position (x,y)
     BattleshipGalactica session;
     int x;
     int y;
-    
+
+    /**
+     * Construct handler object with the driver, and click position.
+     */
     public BtnPlaceShipHandler(BattleshipGalactica session, int x, int y){
         this.session = session;
         this.x = x;
         this.y = y;
     }
-    
+
+    /**
+     * Override the handle method; places ship, if all have been placed the next scene is initiated.
+     */
     @Override
     public void handle(ActionEvent event){
         
