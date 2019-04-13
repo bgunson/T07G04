3f5c8549da3638ee6209a72ec40a8d2bc73a8292
ArcaneShift <47360728+ArcaneShift@users.noreply.github.com>
2019-04-12 22:08:08 -0600

    Update AiPlayer.java

--- gui/SG/src/logic/AiPlayer.java
+++ gui/SG/src/logic/AiPlayer.java
@@ -8,12 +8,7 @@ public class AiPlayer {
 	private Random rand = new Random();
 	private int coordX;
 	private int coordY;
-	static boolean hitLastTurn = false;
-	private boolean shipDestroyedLastTurn = false;
-	static int lastTurnX;
-	static int lastTurnY;
-	static int lastHitX;
-	static int lastHitY;
+
 	/**
 	 * shipPlacer will randomly place the default ships for the computer player n its own board.
 	 * @param board the board object representing the AI's ship locations.
@@ -42,17 +37,8 @@ public class AiPlayer {
      * When called, computer player will shoot on the board (randomly?)...
      */
     public void turn(HitsBoard enemyBoard){
-    	
-    	if(hitLastTurn == true) 
-    	{
-    		System.out.println("\n\n\n\nWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
-    		shootLastShotHit(enemyBoard);
-    		
-    	}
-    	else 
-    	{
+
     		shootNoHit(enemyBoard);
-    	}
     	
     	
     }
@@ -65,14 +51,7 @@ public class AiPlayer {
     	this.coordX = rand.nextInt(10);
     	this.coordY = rand.nextInt(10);
     }
-    public void setCoordX(int x) 
-    {
-    	this.coordX = x;
-    }
-    public void setCoordY(int y) 
-    {
-    	this.coordY = y;
-    }
+
     
     /**
      * When called the x coordinate is returned
@@ -89,36 +68,11 @@ public class AiPlayer {
     {
     	return this.coordY;
     }
-    public void setlastTurnCoords() 
-    {
-    	AiPlayer.lastTurnX = getCoordX();
-    	AiPlayer.lastTurnY = getCoordY();
-    	if(AiPlayer.hitLastTurn == true) 
-    	{
-        	AiPlayer.lastHitX = getCoordX();
-        	AiPlayer.lastHitY = getCoordY();
-    	}
-    }
-    public int getlastTurnX() 
-    {
-    	return AiPlayer.lastTurnX;
-    }
-    public int getlastTurnY() 
-    {
-    	return AiPlayer.lastTurnY;
-    }
-    public int getlastHitX() 
-    {
-    	return AiPlayer.lastHitX;
-    }
-    public int getlastHitY() 
-    {
-    	return AiPlayer.lastHitY;
-    }
+
+ 
     
 	/**
-	 * shootNoHit is one of the two shooting mechanisms of the ai. This is called when the last turn no ship was hit
-	 * and based on that it shoots a random spot of the board (making sure it hasn't been shot at before)
+	 * shootNoHit is the shooting mechanisms of the ai. This is called when the ai wants to shoot
 	 * @param enemyBoard, uses this board to check if the spot it will shoot at is good
 	 */
     public void shootNoHit(HitsBoard enemyBoard) 
@@ -139,101 +93,6 @@ public class AiPlayer {
     	}
     	
 
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-	/**
-	 * shootLastShotHit is one of the two shooting mechanisms of the ai. This is called when the last turn a ship was hit
-	 * and based on that it shoots around the area of that hit spot
-	 * @param enemyBoard, uses this board to check if the spot it will shoot at is good
-	 * @return, returns true or false
-	 */
-    public void shootLastShotHit(HitsBoard enemyBoard) 
-    {
-    	//Condition is used so that coords will be rechosen until they work.
-    	boolean condition = true;
-		int x = getlastTurnX();
-		int y = getlastTurnY();
-		boolean switchHori = false;
-		boolean switchDirection = false;
-		boolean validHitSwitch = false;
-		int placeHolderX = getlastHitX();
-		int placeHolderY = getlastHitY();
-		
-    	while(condition == true) 
-    	{
-    		if(validHitSwitch == false) 
-    		{
-
-        		if(y < 9 && switchHori == false && switchDirection == false) 
-        		{
-        			y++;
-        		}
-        		else if(y == 9 && switchHori == false && switchDirection == false) 
-        		{
-        			switchDirection = true;
-        			y = placeHolderY;
-        		}
-        		else if(y > 0 && switchHori == false && switchDirection == true)
-        		{
-
-        			y--;
-        		}
-        		
-        		else if(y == 0 && switchHori == false && switchDirection == true) 
-        		{
-        			switchHori = true;
-        		}
-        		
-        		
-        		
-        		if(x < 9 && switchHori == true) 
-        		{
-        			x++;
-        		}
-        		else if(x == 9 && switchHori == true && switchDirection == false) 
-        		{
-        			switchDirection = true;
-        			x = placeHolderX;
-        		}
-        		else if(x > 0 && switchHori == true && switchDirection == true)
-        		{
-
-        			x--;
-        		}
-        		
-        		else if(y == 0 && switchHori == true && switchDirection == true) 
-        		{
-        			validHitSwitch = true;
-        		}
-    		}
-    		else 
-    		{
-    			setCoords();
-    			x = getCoordX();
-    			y = getCoordY();
-    		}
-    		
-    		boolean shot = enemyBoard.checkShot(x, y);
-    		
-    		if(shot == true) 
-    		{
-    			System.out.println(x + " " + y);
-    			condition = false;
-    			
-    		}
-    		setCoordX(x);
-    		setCoordY(y);
-    	}
-
-    	
     }
 
 }
