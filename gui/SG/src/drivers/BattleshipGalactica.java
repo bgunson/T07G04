53b0c6f03c142ec98b2dd86e9b37876aa26b6a8c
ArcaneShift <47360728+ArcaneShift@users.noreply.github.com>
2019-04-12 22:18:14 -0600

    Update BattleshipGalactica.java

--- gui/SG/src/drivers/BattleshipGalactica.java
+++ gui/SG/src/drivers/BattleshipGalactica.java
@@ -12,24 +12,34 @@ public class BattleshipGalactica extends Application{
     Game game = new Game();
     private AudioController backgroundMusic = new AudioController(BattleshipGalactica.class.getResource("/Resources/Audio/thing.wav").toExternalForm());
     
+	
+      /**
+     *  Takes scene as a parameter then sets stage to that scene
+     */
     public void setScene(Scene scene){
     	
         stage.setScene(scene);
     }
-    
+      /**
+     *  Is used to start the game after pressing start, additionally stops the menu music from playing
+     */
     public Game getGame(){
     	backgroundMusic.stop();
         return game;
     }
-    
+     /**
+     *  sets the game variable
+     */
     public void setGame(Game game) {
 
     	this.game = game;
     }
-    
+    /**
+     *  When the game is first launched it plays the menu music and loads the stage
+     */
     public void start(Stage stage){
 		
-		backgroundMusic.playSong();
+	backgroundMusic.playSong();
     	
         this.stage = stage;
         stage.setTitle("BATTLESHIP GALACTICA THE IV");
